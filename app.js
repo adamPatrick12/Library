bb6d0f02805fbfe95283a61ac14df1823f363a4d
Adam Patrick <adam_77paper@hotmail.com>
2021-10-25 00:16:38 -0400

    Add more functionality W.I.P

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 
-closebtn =  document.querySelector(".closeBtn")
+closebtn =  document.querySelector(".closeBtn")           //Selecting elements
 addBtn = document.querySelector(".btnAdd")
 bookForm =  document.getElementById("book-form")
 btnSubmit = document.querySelector(".btnSubmit")
 bookCard = document.querySelector(".books")
 title = document.querySelector(".title")
 screenContainer = document.querySelector(".container")
+let btnId = 0;
 
-let myLibrary = [];
 
-  function Book (title, author, pages, read){
+let myLibrary = [];           //Library Array 
+
+  function Book (title, author, pages, read){               //Constructor Function
     this.title = title
     this.author = author
     this.pages = pages
@@ -22,7 +24,7 @@ let myLibrary = [];
 let newBook = new Book()
 
 function addBookToLibrary(){
-  newBook.title = document.getElementById("title").value;
+  newBook.title = document.getElementById("title").value;             //Filling Book object with User Input
   newBook.author = document.getElementById("author").value;
   newBook.pages = document.getElementById("pages").value;
   newBook.read = document.getElementById("read").value;
@@ -32,29 +34,43 @@ function displayBook (){
   newBookCard = document.createElement("div")
   newBookCard.classList.add("books")
   screenContainer.appendChild(newBookCard)
+  removeBookBtn = document.createElement("button")
+  removeBookBtn.classList.add('removeBtn')
+  removeBookBtn.textContent = "Remove"
+  removeBookBtn.value = btnId;
+
   
   for(let i = 0; i< myLibrary.length; i++){
      titleName = document.createElement("p")
-     titleName.classList.add("bookFonts")
-     titleName.textContent = `Title: ${myLibrary[i].title}`
+     titleName.classList.add("titleFonts")
+     titleName.textContent = `${myLibrary[i].title}`
 
      authorName = document.createElement("p")
      authorName.classList.add("bookFonts")
-     authorName.textContent = `Author: ${myLibrary[i].author}`
+     authorName.textContent = `By: ${myLibrary[i].author}`
 
      pageNumber = document.createElement("p")
      pageNumber.classList.add("bookFonts")
      pageNumber.textContent = `Pages: ${myLibrary[i].pages}`
 
      readOrNot = document.createElement("p")
-     readOrNot.classList.add("bookFonts")
+     readOrNot.classList.add("readFonts")
      readOrNot.textContent = `Read: ${myLibrary[i].read}`
   }
   newBookCard.appendChild(titleName); 
   newBookCard.appendChild(authorName); 
   newBookCard.appendChild(pageNumber); 
   newBookCard.appendChild(readOrNot); 
+  newBookCard.appendChild(removeBookBtn); 
   newBookCard.style.display = "block"
+  btnId++
+
+  removeBookBtn.addEventListener('click', (e) =>{
+    c = screenContainer.childNodes
+    console.log(c)
+    
+  });
+
 }
 
 btnSubmit.addEventListener('click', function(e){
@@ -67,6 +83,7 @@ btnSubmit.addEventListener('click', function(e){
   displayBook ()
 });
    
+
 
 addBtn.addEventListener('click', function(){
     openForm()
